8b0b28c9f94634a9d050ec7af1d9dfe0d73ea30e
Matt Holmes <matthewholmes95@yahoo.co.uk>
2021-03-02 14:19:39 +0000

    Fixed issues with if/else

--- front-grill.js
+++ front-grill.js
@@ -4,10 +4,16 @@ console.log("I have created a set of instructions to help guide you through.")
 function frontGrill (food, spice,) {
     if (food = Chicken || Half-Chicken || Quarter-Chicken) {
         console.log ('Cut the chicken into quarters and place any unneeded parts back into the tray.');
-    }
+        console.log('Place the ' + food + ' onto the grill skin side down and leave for one to two minutes.');
+        console.log('If the ' + food + ' has grill marks, turn 90 degrees and leave for another one to two minutes.');
+        console.log('When the ' + food + ' is grilled you should see a criss cross shape.');
+        console.log('Flip the ' + food + ' over and baste the skin side with ' + spice + ' baste.');
+        console.log('Insert ' + spice + ' flag and serve!')    
+    } else {
     console.log('Place the ' + food + ' onto the grill skin side down and leave for one to two minutes.');
     console.log('If the ' + food + ' has grill marks, turn 90 degrees and leave for another one to two minutes.');
     console.log('When the ' + food + ' is grilled you should see a criss cross shape.');
     console.log('Flip the ' + food + ' over and baste the skin side with ' + spice + ' baste.');
     console.log('Insert ' + spice + ' flag and serve!')
+}
 }
